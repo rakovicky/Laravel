a4a8cd5dd2c986cdfca5e443e701a5610b4fa7b8
Martin Rakovicky <rakovicky.martin@gmail.com>
2018-02-06 23:26:49 +0100

    Revert "After 10. tutorial"
    This reverts commit 677db39085b4e5c7703aaa727f540b37c2d78b40.

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,3 @@ Route::get('contact', 'PagesController@getContact');
 Route::get('about', 'PagesController@getAbout');
 
 Route::get('/', 'PagesController@getIndex');
-Route::resource('posts', 'PostController');
-
